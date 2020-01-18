27c1efe918ff56c1ad9656855e1dd2f57a579555
rdowd <rjarrettdowd@yahoo.com>
2020-01-17 19:11:33 -0500

    Made a data to map through for links

---
+++ apple-nav/src/data.js
@@ -0,0 +1,208 @@
+
+
+export default [
+    {
+        title: "Mac",
+        path: "/sub-nav/mac/",
+        links: [
+            {
+                name: "MacBook Air",
+                img: "https://www.apple.com/v/mac/home/am/images/familybrowser/macbookair__ej39du0gz4uq_large.svg",
+                path: "/mac-book-air/",
+            },
+            {
+                name: 'MacBook Pro 13"',
+                img: "https://www.apple.com/v/mac/home/am/images/familybrowser/macbook_pro_13__ep96x983izyq_large.svg",
+                path: "/mac-book-pro-13/",
+            },
+            {
+                name: 'MacBook Pro 16"',
+                img: "https://www.apple.com/v/mac/home/am/images/familybrowser/macbook_pro_16__ni9nkbyq2dm6_large.svg",
+                path: "/mac-book-pro-16/",
+            },
+            {
+                name: 'iMac',
+                img: "https://www.apple.com/v/mac/home/am/images/familybrowser/imac__dkswmjfqeziq_large.svg",
+                path: "/imac/",
+            },
+            {
+                name: 'iMac Pro',
+                img: "https://www.apple.com/v/mac/home/am/images/familybrowser/imac_pro__eer4bfwlutme_large.svg",
+                path: "/imac-pro",
+            },
+        ]
+
+    },
+    {
+        title: "iPad",
+        path: "/sub-nav/ipad/",
+        links:[
+            {
+                name: "iPad Pro",
+                img: "https://www.apple.com/v/ipad/home/aw/images/chapternav/ipadpro_light__dxq7nxjgsd2e_large.svg",
+                path: "/ipad-pro",
+            },
+            {
+                name: "iPad Air",
+                img: "https://www.apple.com/v/ipad/home/aw/images/chapternav/ipadair_light__gan6yfrffc66_large.svg",
+                path: "/ipad-air",
+            },
+            {
+                name: "iPad",
+                img: "https://www.apple.com/v/ipad/home/aw/images/chapternav/ipad_light__expohijp19si_large.svg",
+                path: "/ipad/",
+            },
+            {
+                name: "iPad mini",
+                img: "https://www.apple.com/v/ipad/home/aw/images/chapternav/ipadmini_light__bxpulkud30s2_large.svg",
+                path: "/ipad-mini/",
+            },
+            {
+                name: "iPadOS",
+                img: "https://www.apple.com/v/ipad/home/aw/images/chapternav/ipados_light__f5xv7yr98tm6_large.svg",
+                path: "/ipad-os/"
+            },
+        ]
+    },
+    {
+        title: "iPhone",
+        path: "/sub-nav/iphone/",
+        links:[
+            {
+                name: "iPhone 11 Pro",
+                img: "https://www.apple.com/v/iphone/home/af/images/chapternav/iphone_11_pro_dark__cchxb3bv66vm_large.svg",
+                path: "/iphone-11-pro/",
+            },
+            {
+                name: "iPhone 11",
+                img: "https://www.apple.com/v/iphone/home/af/images/chapternav/iphone_11_dark__exagpxb8zh4y_large.svg",
+                path: "/iphone-11/"
+            },
+            {
+                name: "iPhone XR",
+                img: "https://www.apple.com/v/iphone/home/af/images/chapternav/iphone_xr_dark__gmkx1gdvqqum_large.svg",
+                path: "/iphone-xr/"
+            },
+            {
+                name: "iPhone 8",
+                img: "https://www.apple.com/v/iphone/home/af/images/chapternav/iphone_8_dark__d4f1b8j5cwia_large.svg",
+                path: "/iphone-8/",
+            },
+            {
+                name: "iOS 13",
+                img: "https://www.apple.com/v/iphone/home/af/images/chapternav/iphone_ios_dark__bb9g9sts3q76_large.svg",
+                path: "/ios-13/",
+            },
+        ]
+    },
+    {
+        title: "Watch",
+        path: "/sub-nav/watch/",
+        links: [
+            {
+                name: "Apple Watch Series 5",
+                img: "https://www.apple.com/v/watch/home/x/images/overview/watch_nav_series_2__bk595ih4bliu_large.svg",
+                path: "/watch-5/",
+            },
+            {
+                name: "Apple Watch Nike",
+                img: "https://www.apple.com/v/watch/home/x/images/overview/watch_nav_nike__mkjgkrrlq2ye_large.svg",
+                path: "/watch-nike/",
+            },
+            {
+                name: "Apple Watch Hermes",
+                img: "https://www.apple.com/v/watch/home/x/images/overview/watch_nav_hermes__b1zeoxbn0df6_large.svg",
+                path: "/watch-hermes/",
+            },
+            {
+                name: "Apple Watch Series 3",
+                img: "https://www.apple.com/v/watch/home/x/images/overview/watch_nav_edition__botzv6l1ftua_large.svg",
+                path: "/watch-3/",
+            },
+            {
+                name: "watchOS",
+                img: "https://www.apple.com/v/watch/home/x/images/overview/watch_nav_watch_os__fctkp231ik6e_large.svg",
+                path: "/watch-os",
+            },
+        ]
+    },
+    {
+        title: "TV",
+        path: "/sub-nav/tv/",
+        links: [
+            {
+                name: "Apple TV app",
+                img: "https://www.apple.com/v/tv/home/l/images/chapternav/apple_tv_app_dark__blle10nnpq3m_large.svg",
+                path: "/apple-tv-app/",
+            },
+            {
+                name: "Apple TV+",
+                img: "https://www.apple.com/v/tv/home/l/images/chapternav/apple_tv_plus_dark__fntbqaxjuouy_large.svg",
+                path: "/apple-tv/",
+            },
+            {
+                name: "Apple TV 4K",
+                img: "https://www.apple.com/v/tv/home/l/images/chapternav/apple_tv_4K_dark__fr7ph83oiju6_large.svg",
+                path: "/apple-tv-4k/",
+            },
+            {
+                name: "Apple TV HD",
+                img: "https://www.apple.com/v/tv/home/l/images/chapternav/apple_tv_hd_dark__68lr0x9ju8yu_large.svg",
+                path: "/apple-tv-hd/"
+            },
+            {
+                name: "AirPlay",
+                img: "https://www.apple.com/v/tv/home/l/images/chapternav/apple_tv_airplay_dark__2ljjityj6kye_large.svg",
+                path: "/air-play/",
+            },
+            {
+                name: "Accessories",
+                img: "https://www.apple.com/v/tv/home/l/images/chapternav/apple_tv_accessories_dark__e7bbr1ui45me_large.svg",
+                path: "/tv-accessories",
+            },
+        ]
+    },
+    {
+        title: "Music",
+        path: "/sub-nav/music/",
+        links: [
+            {
+                name: "Apple Music",
+                img: "https://www.apple.com/v/music/l/images/chapternav/apple_music_dark__e7el1dqc7ouq_large.svg",
+                path: "/apple-music/",
+            },
+            {
+                name: "AirPods Pro",
+                img: "https://www.apple.com/v/music/l/images/chapternav/airpods_pro_dark__fq982ukhrn2a_large.svg",
+                path: "/air-pods-pro/",
+            },
+            {
+                name: "AirPods",
+                img: "https://www.apple.com/v/music/l/images/chapternav/airpods_dark__ev9iwonn6526_large.svg",
+                path: "/air-pods/",
+            },
+            {
+                name: "HomePod",
+                img: "https://www.apple.com/v/music/l/images/chapternav/homepod_dark__rdq3959n9iqq_large.svg",
+                path: "/home-pod/",
+            },
+            {
+                name: "iPod touch",
+                img: "https://www.apple.com/v/music/l/images/chapternav/ipod_touch_dark__cnll1unb2no2_large.svg",
+                path: "/ipod-touch",
+            },
+            {
+                name: "Music Accessories",
+                img: "https://www.apple.com/v/music/l/images/chapternav/music_accessories_dark__beuh96rrvbwy_large.svg",
+                path: "/music-accessories/"
+            },
+            {
+                name: "Gift Cards",
+                img: "https://www.apple.com/v/music/l/images/chapternav/gift_cards_dark__fhhcjey7942u_large.svg",
+                path: "/gift-cards/",
+            },
+
+
+        ]
+    },
+];
